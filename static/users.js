4bc7d6a36ae23f5ed172c6fc623604aa6f0f4e35
Marceau-h <git@marceau-h.fr>
2024-05-02 15:04:29 +0200

    Badge infos on hover

---
+++ static/users.js
@@ -0,0 +1,32 @@
+let imgs_hover = {};
+let url = "/static/imgs_hover.json";
+
+let promise = fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        imgs_hover = data;
+    }
+    );
+console.log(imgs_hover);
+
+async function isJsonLoaded() {
+    return promise.then(() => {
+        return true;
+
+    });
+}
+
+
+async function generateHover(img) {
+    if (await isJsonLoaded()) {
+        const parentDiv = img.parentElement;
+        const accompanyingP = parentDiv.querySelector('p');
+        const id = parentDiv.id;
+        const text = imgs_hover[id];
+        accompanyingP.innerHTML = text;
+    }
+    else {
+        console.log("Json not loaded yet");
+    }
+}
+
